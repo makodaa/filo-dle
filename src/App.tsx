a9658a6acc9e7d2fde2f671c20bafc7915690db4
makodaa <qmkbcerenio@tip.edu.ph>
2025-10-06 21:07:37 +0800

    Initial setup for pair development

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Test } from "./components/test";
+function App() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <Test />
+            </div>
+        </>
+    );
+}
+
+export default App;
